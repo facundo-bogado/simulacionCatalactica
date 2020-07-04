7a062599ffa6615bb8c4506b8a4fc8f22588736c
facubogado <facundobogado@gmail.com>
2020-07-04 04:52:49 -0300

    primer commit

---
+++ main.py
@@ -0,0 +1,13 @@
+class Bien:
+    def __init__(self):
+        #
+        pass
+
+class Individuo:
+    def __init__(self):
+        # BIENES => [(id, aprecio)...]
+        # NEEDS => [(id, aprecio)...]
+        # INGRESO => precio unidades monetarias
+        self.BIENES = []
+        self.NEEDS = []
+        
